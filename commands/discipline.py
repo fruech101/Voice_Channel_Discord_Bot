9045dbb08b2abaa96efb59248e1420c646c9ad78
wutnow13 <66490530+wutnow13@users.noreply.github.com>
2020-09-17 12:42:32 -0500

    Enter

--- commands/discipline.py
+++ commands/discipline.py
@@ -23,7 +23,10 @@ async def command( client, message, name ):
     send_message( message, "Message Deleted" )
     
     #Open the file, and unpack contents
-    f = open( "discipline.txt", "w" )
+    try:
+        f = open( "discipline.txt", "r" )
+    except:
+        f = open( "discipline.txt", "x" )
     send_message( message, "File Opened" )
     data = unpack_file( f )
     send_message( message, "File Unpacked" )
